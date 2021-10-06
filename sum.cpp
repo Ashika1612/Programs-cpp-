6f9aedb03d0aa6cea252f23b1f5730daa057278a
Ashika1612 <90318556+Ashika1612@users.noreply.github.com>
2021-10-06 19:52:23 +0530

    created sum.cpp
    finds the sum of two numbers.

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"ENTER THE FIRST NUMBER: ";
+	cin>>a;
+	cout<<"ENTER THE SECOND NUMBER: ";
+	cin>>b;
+	system("CLS");
+	c=a+b;
+	cout<<"\nTHE PRODUCT IS "<<c;
+}
